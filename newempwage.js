6a258d6071d14d967a3a1e58138574560c5323b8
satya-patel <satyaprakashpatel1998@gmail.com>
2021-05-25 12:51:55 +0530

    uc3: refactor added

--- newempwage.js
+++ newempwage.js
@@ -1,35 +1,29 @@
-//uc1
-
-    
-    const IS_ABSENT = 0;
+const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empcheck = Math.floor(Math.random() * 10) % 2;
-if (empcheck == IS_ABSENT) {
-    console.log("uc1 - employee is ABSENT");
-
-} else{
-    console.log("uc1 - employee is present");
-}
-//uc2
-
 let empHrs = 0;
 empCheck = Math.floor(Math.random()*10)%3;
+function getWorkHours(empCheck){
 switch(empCheck){
     case IS_PART_TIME:
         empHrs = PART_TIME_HOURS;
         break;
+        return PART_TIME_HOURS;
     case IS_FULL_TIME:
         empHrs = FULL_TIME_HOURS;
         break;
+        return FULL_TIME_HOURS;
     default:
         empHrs = 0;
+        return 0;
 }
 
+}
+empCheck = Math.floor(Math.random()*10)%3;
+let empHrs = getWorkHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage: "+empWage); 
-
+console.log("Emp Wage: "+empWage);
