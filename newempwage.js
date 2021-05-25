c89c8f9a55d15c1a4ab31bec76f8675593174683
satya-patel <satyaprakashpatel1998@gmail.com>
2021-05-25 12:17:35 +0530

    uc1 :add employee attendance

---
+++ newempwage.js
@@ -0,0 +1,11 @@
+//uc1
+{
+    const IS_ABSENT = 0
+let empcheck = Math.floor(Math.random() * 10) % 2;
+if (empcheck == IS_ABSENT) {
+    console.log("uc1 - employee is ABSENT");
+
+} else{
+    console.log("uc1 - employee is present");
+}
+}
