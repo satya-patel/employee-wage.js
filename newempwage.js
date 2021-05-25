d78717cdc8684a2387db181c48a1ac62b82be2bb
satya-patel <satyaprakashpatel1998@gmail.com>
2021-05-25 12:38:18 +0530

    uc2: calculate emp wage

--- newempwage.js
+++ newempwage.js
@@ -1,6 +1,13 @@
 //uc1
-{
-    const IS_ABSENT = 0
+
+    
+    const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
 let empcheck = Math.floor(Math.random() * 10) % 2;
 if (empcheck == IS_ABSENT) {
     console.log("uc1 - employee is ABSENT");
@@ -8,4 +15,21 @@ if (empcheck == IS_ABSENT) {
 } else{
     console.log("uc1 - employee is present");
 }
+//uc2
+
+let empHrs = 0;
+empCheck = Math.floor(Math.random()*10)%3;
+switch(empCheck){
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS;
+        break;
+    default:
+        empHrs = 0;
 }
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage: "+empWage); 
+
