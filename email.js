5d231bb3142bb0da3cfc8815a5a8a7fe01c31b47
satya-patel <satyaprakashpatel1998@gmail.com>
2021-05-28 16:28:03 +0530

    emailuc2  validate  added

--- email.js
+++ email.js
@@ -1,8 +1,13 @@
-let PinRegex = RegExp('^[a-z]{3,}$');
+let PinRegex = RegExp('^[a-z][0-9a-z_+-]*\\.?[0-9a-z_+-]+@\\w+(\\.[a-z]{2,}){1,2}$');
 
 //UC-1: Check Initial Part
 let email_1 = "abc";
 check_validity(email_1);
+
+//UC-2: Chect @ and Part Latter it.
+let email_2 = "abc@bridgelab";
+check_validity(email_2);
+
 
 function check_validity(sample){
     let output = PinRegex.test(sample);
