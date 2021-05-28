b69a31b97cb1536fe8223fddde47ca7033f2f60b
satya-patel <satyaprakashpatel1998@gmail.com>
2021-05-28 16:48:31 +0530

    pincode uc4 added

--- pincode.js
+++ pincode.js
@@ -1,10 +1,11 @@
 //UC-1: 6 Digit Pin Code
 
-let PinRegex = RegExp('^[1-9]{1}[0-9]{5}$');
+let PinRegex = RegExp('^[1-9]{1}[0-9]{2}[\\s]{0,1}[0-9]{3}$');
 
 let pincode_1 = "400088";
 let pincode_2 = "A400088";
 let pincode_3 = "400088B";
+let pincode_4 = "400 088";
 
 check_validity(pincode_1);
 
@@ -14,6 +15,8 @@ check_validity(pincode_2);
 //UC-3: Restrict Alphabet at Last Place
 check_validity(pincode_3);
 
+//UC-4: Validate Space in the Middle
+check_validity(pincode_4);
 
 function check_validity(sample){
     let output = PinRegex.test(sample);
