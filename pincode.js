8609c953862a11f448f06c7ef2ac6fac418bf9fe
satya-patel <satyaprakashpatel1998@gmail.com>
2021-05-28 16:44:43 +0530

    pincode uc3 added

--- pincode.js
+++ pincode.js
@@ -4,11 +4,15 @@ let PinRegex = RegExp('^[1-9]{1}[0-9]{5}$');
 
 let pincode_1 = "400088";
 let pincode_2 = "A400088";
+let pincode_3 = "400088B";
 
 check_validity(pincode_1);
 
 //UC-2: Restrict Alphabet at First Place
 check_validity(pincode_2);
+
+//UC-3: Restrict Alphabet at Last Place
+check_validity(pincode_3);
 
 
 function check_validity(sample){
